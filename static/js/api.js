a86d9b0a39cd911477eb2ad56e18176996206430
ejn2 <60011590+ejn2@users.noreply.github.com>
2020-11-22 13:14:32 -0300

    Update api.js

--- static/js/api.js
+++ static/js/api.js
@@ -1,6 +1,6 @@
 const api = [
-    { title: "Sansung S7", description: "Test sansung S7" },
-    { title: "Sansung A1", description: "Test sansung A1" },
+    { title: "Samsung S7", description: "Test sansung S7" },
+    { title: "Samsung A1", description: "Test sansung A1" },
     { title: "Motorola G3", description: "Test motorola G3" },
     { title: "Motorola E", description: "Test motorola E" },
     { title: "Lg K10 PL", description: "Test lg k10-pl" },
